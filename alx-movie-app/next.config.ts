39aebd78239f5b5f86877411b77c59692fdb54c2
Dario Didi <dididario.dd2@gmail.com>
2025-07-03 19:25:05 +0300

    add PWA support

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,17 +1,29 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['m.media-amazon.com'],
+  },
 };
 
-module.exports = {
+const withPWA = withPWAInit({
+  dest: 'public'
+})
+
+/*module.exports = {
   images: {
     remotePatterns: [
       new URL('https://moviesdatabase.p.rapidapi.com/titles**'),
       new URL('https://m.media-amazon.com/**')
     ],
   },
-}
+}*/
 
-export default nextConfig;
+//export default nextConfig;
+export default withPWA({
+  ...nextConfig
+})
